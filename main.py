5077aa7fce5d88487ae824d38d3b9a7047fb8d9e
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-12-15 09:42:49 -0600

    update to main.py fix no post 500 error

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 api_key = os.getenv("API_KEY")
 @app.route("/", methods=["GET", "POST"])
 def index():
+    weather_data = {'city': "New York", 'temperature': 'Temp 44.13F', 'wind': 'Wind 6.38 MPH', 'humid': 'Humidity 67', 'icon': 'https://openweathermap.org/img/w/04n.png'}
     if request.method == "POST":
         city = request.form["city_input"]
-        weather_data = get_weather(city)
-        print("weather", weather_data)
+        weather_data = {'city': city, 'temperature': 'Temp 44.13F', 'wind': 'Wind 6.38 MPH', 'humid': 'Humidity 67', 'icon': 'https://openweathermap.org/img/w/04n.png'}
+        # weather_data = get_weather(city)
         return render_template("index.html", weather_data=weather_data)
-    return render_template("index.html")
+    return render_template("index.html",weather_data=weather_data)
 
 def get_weather(city):
     weather_geo_url = f"https://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={api_key}"
@@ -32,3 +33,6 @@ def get_weather(city):
         "icon": f'https://openweathermap.org/img/w/{weather_data_response["list"][0]["weather"][0]["icon"]}.png'
     }
     return weather_data_json
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
