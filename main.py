d2ba11aca9f2c5a4493d1452c2bc4073de194792
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-12-14 20:21:09 -0600

    update index.html to get all weather data, issue with history buttons going beyond 2

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ def get_weather(city):
     weather_data_response = response_weather.json()
     weather_data_json = {
         "city": geo_data_response[0]["name"],
-        "temperature": weather_data_response["list"][0]["main"]["temp"],
-        "description": weather_data_response["list"][0]["weather"][0]["description"],
+        "temperature": f'Temp {weather_data_response["list"][0]["main"]["temp"]}F',
+        "wind": f'Wind {weather_data_response["list"][0]["wind"]["speed"]} MPH',
+        "humid": f'Humidity {weather_data_response["list"][0]["main"]["humidity"]}',
         "icon": f'https://openweathermap.org/img/w/{weather_data_response["list"][0]["weather"][0]["icon"]}.png'
     }
     return weather_data_json
--- static/script.js
+++ static/script.js
@@ -4,30 +4,33 @@ $(function () {
     let event;
     let city_name;
     // Checks if there is already content in the local storage `#history_0` if not will set it to `New York`
-    // if(localStorage.getItem("#history_0") == null){
-    //     $("#city").text(city_name)
-    // }else{
-    //     // If `#history_0` has content will display it
-    //     $("#history_0").text(localStorage.getItem("#history_0"))
-    //     // $("#city").text(localStorage.getItem("#history_0"))
-    //     i++;
-    // }
+    if(localStorage.getItem("#history_0") == null){
+        $("#city").text(city_name)
+    }else{
+        // If `#history_0` has content will display it
+        $("#history_0").text(localStorage.getItem("#history_0"))
+        // $("#city").text(localStorage.getItem("#history_0"))
+        i++;
+    }
+    
     // Event listener for the search button to be clicked
     $("#search_btn").on("click", function(){
         // Sets event to the value of the entered in text in search field
         event = $(this).siblings("#city_input").val();
-        if(event){
+        console.log('event', event);
+        if(event != null){
             city_name = event
             // get_weather()
             // Set text content for the document elements "#city" and "#history_{i}"
             // i being the variable i
-            $("#city").text(event)
+            // $("#city").text(event)
             $(`#history_${i}`).text(`${event}`)
             // Sets the local storage
             localStorage.setItem(`#history_${i}`,event);
             $(`#history_${i}`).removeClass("invisible").addClass("visible");
             // Increaments the value of i by 1
             i++
+            console.log("i",i);
             if(i>4){
                 i = 0
             }
@@ -39,10 +42,10 @@ function show_local(){
     const num_elements = 5
     // For loop that iterates through each history storage keys
     // The changes the document display for the storage to visible if it is not null
-    for(let i = 0; i < num_elements; i++){
-        $(`#history_${i}`).text(localStorage.getItem(`#history_${i}`))
-        if(localStorage.getItem(`#history_${i}`) != null){
-            $(`#history_${i}`).removeClass("invisible").addClass("visible");
+    for(let q = 0; q < num_elements; q++){
+        $(`#history_${q}`).text(localStorage.getItem(`#history_${q}`))
+        if(localStorage.getItem(`#history_${q}`) != null){
+            $(`#history_${q}`).removeClass("invisible").addClass("visible");
             }
         }
     };
