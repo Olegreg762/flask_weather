becb510276acd84db6225235f8bd8a2c9a857969
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-12-23 05:26:21 -0600

    add error handling to get_weather and error image

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ api_key = os.getenv("API_KEY")
 def before_request():
     if 'history' not in session:
         session['history'] = {
-            0: {'visibility': 'invisible', 'city': ''},
-            1: {'visibility': 'invisible', 'city': ''},
-            2: {'visibility': 'invisible', 'city': ''},
-            3: {'visibility': 'invisible', 'city': ''},
-            4: {'visibility': 'invisible', 'city': ''}
+            0: {'visibility': 'invisible', 'city': '', 'region': ''},
+            1: {'visibility': 'invisible', 'city': '', 'region': ''},
+            2: {'visibility': 'invisible', 'city': '', 'region': ''},
+            3: {'visibility': 'invisible', 'city': '', 'region': ''},
+            4: {'visibility': 'invisible', 'city': '', 'region': ''}
         }
     if 'counter' not in session:
         session['counter'] = int(0)
@@ -31,7 +31,7 @@ def index():
         city = request.form["city_input"]
         region = request.form["region_select"]
         weather_data = get_weather(city, region)
-        update_session_variables(city)
+        update_session_variables(city, region)
         history = session['history']
         return render_template("index.html", weather_data=weather_data, history=history, options=us_regions)
     weather_data = get_weather('Chicago', 'illinois')
@@ -39,38 +39,53 @@ def index():
     return render_template("index.html",weather_data=weather_data, history=history, options=us_regions)
 
 def get_weather(city, region):
-    weather_geo_url = f"https://api.openweathermap.org/geo/1.0/direct?q={city},{region}&appid={api_key}"
-    response_geo = requests.get(weather_geo_url)
-    geo_data_response = response_geo.json()
-
-    lat = geo_data_response[0]['lat']
-    lon = geo_data_response[0]['lon']
- 
-    weather_data_url = f"https://api.openweathermap.org/data//2.5/forecast?units=imperial&lat={lat}&lon={lon}&appid={api_key}"
-    response_weather = requests.get(weather_data_url)
-    weather_data_response = response_weather.json()
     weather_data_json = {}
-    for i in range(6):
-        jd = i * 8
-        if jd == 40:
-            jd -= 1
-        date = datetime.utcfromtimestamp(weather_data_response["list"][jd]["dt"])
-        formatted_date = date.strftime("%m/%d/%Y")
-        weather_data_json.update({
-        f"city{i}": geo_data_response[0]["name"],
-        f"date{i}" : formatted_date,
-        f"temperature{i}": f'Temp {weather_data_response["list"][jd]["main"]["temp"]}F',
-        f"wind{i}": f'Wind {weather_data_response["list"][jd]["wind"]["speed"]} MPH',
-        f"humid{i}": f'Humidity {weather_data_response["list"][jd]["main"]["humidity"]}',
-        f"icon{i}": f'https://openweathermap.org/img/w/{weather_data_response["list"][jd]["weather"][0]["icon"]}.png'
-        })
+    try:
+        weather_geo_url = f"https://api.openweathermap.org/geo/1.0/direct?q={city},{region}&appid={api_key}"
+        response_geo = requests.get(weather_geo_url)
+        geo_data_response = response_geo.json()
+
+        lat = geo_data_response[0]['lat']
+        lon = geo_data_response[0]['lon']
+    
+        weather_data_url = f"https://api.openweathermap.org/data//2.5/forecast?units=imperial&lat={lat}&lon={lon}&appid={api_key}"
+        response_weather = requests.get(weather_data_url)
+        weather_data_response = response_weather.json()
+        for i in range(6):
+            jd = i * 8
+            if jd == 40:
+                jd -= 1
+            date = datetime.utcfromtimestamp(weather_data_response["list"][jd]["dt"])
+            formatted_date = date.strftime("%m/%d/%Y")
+            weather_data_json.update({
+            f"city{i}": geo_data_response[0]["name"],
+            f"date{i}" : formatted_date,
+            f"temperature{i}": f'Temp {weather_data_response["list"][jd]["main"]["temp"]}℉',
+            f"wind{i}": f'Wind {weather_data_response["list"][jd]["wind"]["speed"]} MPH',
+            f"humid{i}": f'Humidity {weather_data_response["list"][jd]["main"]["humidity"]}',
+            f"icon{i}": f'https://openweathermap.org/img/w/{weather_data_response["list"][jd]["weather"][0]["icon"]}.png'
+            })
+    except:
+        error_image = '/static/images/error_image.png'
+        for i in range(6):
+            jd = i * 8
+            if jd == 40:
+                jd -= 1
+            weather_data_json.update({
+                f"city{i}": f'Request Error {response_geo}',
+                f"date{i}" : f'Request Error {response_geo}',
+                f"temperature{i}": f'Request Error {response_geo}',
+                f"wind{i}": f'Request Error {response_geo}',
+                f"humid{i}": f'Request Error {response_geo}',
+                f"icon{i}": error_image
+            })
     return weather_data_json
 
-def update_session_variables(city):
+def update_session_variables(city, region):
     counter = str(session['counter'])
 
     history = session['history']
-    history[counter].update({'visibility': 'visible', 'city': city})
+    history[counter].update({'visibility': 'visible', 'city': city, 'region': region})
 
     counter = int(counter)
     counter += 1
